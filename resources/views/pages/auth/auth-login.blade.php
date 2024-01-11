039a5d67ad99e6eeb253d8628f65c8ac0c52bef7
kalfinbagas <kalfinbagas2@gmail.com>
2024-01-11 23:07:05 +0700

    crud user

--- resources/views/pages/auth/auth-login.blade.php
+++ resources/views/pages/auth/auth-login.blade.php
@@ -16,21 +16,25 @@
 
         <div class="card-body">
             <form method="POST"
-                action="#"
+                action="{{ route('login') }}"
                 class="needs-validation"
                 novalidate="">
+                @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input id="email"
                         type="email"
-                        class="form-control"
+                        class="form-control @error('email') is-invalid @enderror"
                         name="email"
                         tabindex="1"
-                        required
+                        
                         autofocus>
-                    <div class="invalid-feedback">
-                        Please fill in your email
-                    </div>
+                        @error('email')
+                        <div class="invalid-feedback">
+                         {{ $message }}
+                        </div>
+                        @enderror
+                   
                 </div>
 
                 <div class="form-group">
@@ -46,26 +50,18 @@
                     </div>
                     <input id="password"
                         type="password"
-                        class="form-control"
+                        class="form-control @error('password') is-invalid @enderror"
                         name="password"
                         tabindex="2"
-                        required>
+                        >
+                        @error('password')
                     <div class="invalid-feedback">
-                        please fill in your password
+                        {{ $message }}
                     </div>
+                    @enderror
                 </div>
 
-                <div class="form-group">
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox"
-                            name="remember"
-                            class="custom-control-input"
-                            tabindex="3"
-                            id="remember-me">
-                        <label class="custom-control-label"
-                            for="remember-me">Remember Me</label>
-                    </div>
-                </div>
+                
 
                 <div class="form-group">
                     <button type="submit"
@@ -75,21 +71,7 @@
                     </button>
                 </div>
             </form>
-            <div class="mt-4 mb-3 text-center">
-                <div class="text-job text-muted">Login With Social</div>
-            </div>
-            <div class="row sm-gutters">
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-facebook">
-                        <span class="fab fa-facebook"></span> Facebook
-                    </a>
-                </div>
-                <div class="col-6">
-                    <a class="btn btn-block btn-social btn-twitter">
-                        <span class="fab fa-twitter"></span> Twitter
-                    </a>
-                </div>
-            </div>
+          
 
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,22 +15,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('pages.app.dashboard-siakad',['type_menu' => '']);
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/login', function () {
-    return view('pages.auth.auth-login');
-})->name('login');
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.app.dashboard-siakad',['type_menu' => '']);
+    })->name('home');
+    Route::resource('user', UserController::class);
+});
 
-Route::get('/forgot', function () {
-    return view('pages.auth.auth-forgot-password');
-})->name('forgot');
+// Route::get('/login', function () {
+//     return view('pages.auth.auth-login');
+// })->name('login');
 
-Route::get('/reset-password', function () {
-    return view('pages.auth.auth-reset-password');
-})->name('reset');
+// Route::get('/forgot', function () {
+//     return view('pages.auth.auth-forgot-password');
+// })->name('forgot');
 
-Route::get('/register', function () {
-    return view('pages.auth.auth-register');
-})-> name('register');
+// Route::get('/reset-password', function () {
+//     return view('pages.auth.auth-reset-password');
+// })->name('reset');
+
+// Route::get('/register', function () {
+//     return view('pages.auth.auth-register');
+// })-> name('register');
 
